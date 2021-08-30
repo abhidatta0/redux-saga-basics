2df1eeec7bf7e6c1dcfddd2b35098865ec434fec
abhidatta0 <abhidatta146@gmail.com>
2021-08-30 21:05:58 +0530

    initial commit

---
+++ src/components/Counter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div>
+            <button>
+                Increment
+            </button>
+            <button>
+                Increment
+            </button>
+            <div>
+                Clicked 0 times
+            </div>
+        </div>
+    )
+}
+
+export default Counter;
